d73b9bb74937d5c5304a997d1e3ac41f65283543
Andreas Hertle <hertle@informatik.uni-freiburg.de>
2017-10-27 16:27:14 +0200

    improved visualization code

--- visualization/src/vis_plan_graph.py
+++ visualization/src/vis_plan_graph.py
@@ -35,8 +35,6 @@ class Visualization:
 		self.action_feed_back_subscriber = rospy.Subscriber(
 			'/kcl_rosplan/action_feedback', ActionFeedback, self.action_feedback_callback)
 		
-		#rospy.sleep(0.3)
-		#self.render_graph()
 		self.timer = rospy.Timer(period=rospy.Duration(0.3), callback=self.schedule_update, oneshot=True)
 
 	def plan_callback(self, msg):
@@ -77,25 +75,37 @@ class Visualization:
 		self.render_graph()
 		
 	def update_params(self):
-		param_names = ['graph_template', 'node_template',
-									'arg_template', 'highlight_keywords', 'show_only_highlighted']
+		param_names = ['graph_template', 'node_template', 'color_text_template',
+									'highlight_keywords', 'show_only_highlighted', 'colorize_highlighted']
 		for param in param_names:
 			self.__dict__[param] = rospy.get_param('~{}'.format(param))
 		
 		# update color tables
 		for keyword in self.highlight_keywords:
 			if keyword not in self.color_table:
-				#color = colorsys.hsv_to_rgb(h=self.calculate_color_hue(len(self.color_table)), s=1.0, v=0.8)
-				#self.color_table[keyword] = '{} {} {}'.format(color[0], color[1], color[2])
-				self.color_table[keyword] = '{} {} {}'.format(self.calculate_color_hue(len(self.color_table)), 1.0, 0.8)
+				hue = self.calculate_color_hue(len(self.color_table))
+				self.color_table[keyword] = '{} {} {}'.format(hue, 1.0, 0.8)
 			
 	def calculate_color_hue(self, index):
-		return (29*float(index)%360.0)/360.0
-			
+		return (29 * float(index) % 360.0) / 360.0
+	
+	def colorize_text(self, text):
+		if len(self.highlight_keywords) == 0:
+			return text
+		if text in self.color_table:
+			if self.colorize_highlighted:
+				return self.color_text_template.format(color=self.color_table[text], arg=text)
+			else:
+				return self.color_text_template.format(color='black', arg=text)
+		else:
+			if self.show_only_highlighted:
+				return ''
+			return self.color_text_template.format(color='grey', arg=text)
+				
+	
 	def render_graph(self):
 		if not self.graph or not self.plan:
 			return
-		rospy.loginfo('start render graph')
 		
 		# action status subgraphs
 		enabled = ' '.join(str(i) for i in self.enabled_action_ids)
@@ -105,21 +115,22 @@ class Visualization:
 		# actions with highlighted key words
 		node_list = []
 		for action in self.plan:
-			arg_list = []
-			if self.highlight_keywords and len(self.highlight_keywords) > 0:
-				for kv in action.parameters:
-					if kv.value in self.highlight_keywords:
-						arg_list.append(self.arg_template.format(color=self.color_table[kv.value], arg=kv.value))
-					elif not self.show_only_highlighted:
-						arg_list.append(kv.value)
-			else:
-				arg_list = [kv.value for kv in action.parameters]
+			arg_list = [self.colorize_text(kv.value) for kv in action.parameters]
+			
+# 			if self.highlight_keywords and len(self.highlight_keywords) > 0:
+# 				for kv in action.parameters:
+# 					if kv.value in self.highlight_keywords:
+# 						arg_list.append(self.arg_template.format(color=self.color_table[kv.value], arg=kv.value))
+# 					elif not self.show_only_highlighted:
+# 						arg_list.append(kv.value)
+# 			else:
+# 				arg_list = [kv.value for kv in action.parameters]
 			args = ' '.join(arg_list)
 			node = self.node_template.format(id=action.action_id, name=action.name, args=args)
 			node_list.append(node)
 		nodes = '\n'.join(node_list)
 		
-		# graph connections
+		# graph connections, a bit hacky...
 		e_start = self.graph.find('"0"')
 		e_end = self.graph.rfind('"')
 		edges = self.graph[e_start:e_end+1]
